559160c14d654b333c8e0f561d984d67e3d18c3b
rawfishc <rawfishc@gitee.com>
2023-04-21 13:44:09 +0800

    开始第三个版本

--- src/main/java/rawfish/artedprvt/script/ScriptProperties.java
+++ src/main/java/rawfish/artedprvt/script/ScriptProperties.java
@@ -14,7 +14,7 @@ public class ScriptProperties {
     private static Map<String,String> props=new HashMap<String,String>(){{
         put("frame.modid","artedprvt");
         put("frame.name","Artedprvt Frame");
-        put("frame.version","1.1@obsidian");
+        put("frame.version","1.2@dustdirt");
         put("frame.author","Rawfishc");
         put("frame.dir",System.getProperties().get("user.dir").toString()+"/artedprvt");
         put("frame.mcversion","1.8.9");
