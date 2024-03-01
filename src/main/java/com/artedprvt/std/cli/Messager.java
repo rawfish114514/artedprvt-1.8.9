fc7dd34342adb7c97fca9bdd017a1ce9fcf4956c
rawfish <github.com/rawfish114514>
2024-03-01 23:27:31 +0800

    .

--- src/main/java/com/artedprvt/std/cli/Messager.java
+++ src/main/java/com/artedprvt/std/cli/Messager.java
@@ -3,8 +3,7 @@ package com.artedprvt.std.cli;
 import com.artedprvt.api.Solvable;
 
 /**
- * 信使?
- * 作为命令的实现者 并不关心使用何种交互界面(控制台 游戏聊天栏 其他控制台实现)
+ * 传递消息
  */
 @Solvable
 public interface Messager {
@@ -36,9 +35,8 @@ public interface Messager {
 
     /**
      * 提供临时的交互的对话框和按钮
-     * 要求阻塞当前线程直到得到结果
+     * 实现方法应该阻塞当前线程直到得到结果
      * 对话框应该一直显示在上层
-     * 返回一个字符串表示结果
      * 返回buttons的索引表示用户按下的按钮
      * 如果返回-1则表示用户没有选择按下任何按钮
      *
